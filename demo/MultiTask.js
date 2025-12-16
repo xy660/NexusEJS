1a2932ff997564b0b697d8ad706e786a56fac8ec
xy660 <73594017+xy660@users.noreply.github.com>
2025-12-16 17:00:06 +0800

    Refactor MultiTask.js for task identification and delay
    Updated task execution to use loop index for task identification and added a delay before returning the counter result.

--- demo/MultiTask.js
+++ demo/MultiTask.js
@@ -1,13 +1,15 @@
 let counter = {cnt:0};
 for(let i = 0;i < 50;i++){
-    let p = runTask(x=>{
-        println("task" + x.id);
+    let p = runTask(()=>{
+        println("task" + i);
         delay(500);
-        lock(x.obj){ //lock statement use native mutex to protect access
-            x.obj.cnt++;
+        lock(counter){ //lock statement use native mutex to protect access
+            counter.cnt++;
         };
-        return x.obj.cnt;
-    },{id:i,obj:counter})
+        return counter.cnt;
+    })
 }
 
-return counter.cnt;
+delay(5000);
+
+return counter.cnt; //the result is 50
