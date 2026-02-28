e329967be7abcf94390aa7777704da72c2def966
xy660 <73594017+xy660@users.noreply.github.com>
2026-02-28 17:57:13 +0800

    修复ESP32 PWM桥接层的拼接错误
    1.修复ESP8266 CC1编译器对lambda的返回值处理导致无法编译问题
    2.修复ESP32 PWM桥接层的拼接错误问题

--- src/NexusEJS-Firmware/src/ESP32Driver/ESP32_Driver_PWM.cpp
+++ src/NexusEJS-Firmware/src/ESP32Driver/ESP32_Driver_PWM.cpp
@@ -15,18 +15,18 @@ VMObject* pwmChannelObjects = (VMObject*)pwmChannelObjectsBuffer;
 void InitPWMChannels(){
     for(int i = 0;i < MAX_PWM_CHANNELS;i++){
         new (&pwmChannelObjects[i]) VMObject(ValueType::OBJECT);
-        pwmChannelObjects[i].implement.objectImpl["channe"] = CreateNumberVariable(i);
+        pwmChannelObjects[i].implement.objectImpl["channel"] = CreateNumberVariable(i);
         pwmChannelObjects[i].implement.objectImpl["frequency"] = CreateNumberVariable(1000);
         pwmChannelObjects[i].implement.objectImpl["resolution"] = CreateNumberVariable(10);
         //PWMObject.attach(pin);
         pwmChannelObjects[i].implement.objectImpl["attach"] = VM::CreateSystemFunc(1,[](std::vector<VariableValue>& args, VMObject* thisValue,VMWorker* currentWorker) -> VariableValue{
             if(args[0].getContentType() != ValueType::NUM){
-                currentWorker->ThrowError("PWM.attach only accept number");
+                currentWorker->ThrowError("PWM.attach(pin) only accept number");
                 return VariableValue();
             }
             uint32_t freq = thisValue->implement.objectImpl["frequency"].content.number;
             uint32_t res = thisValue->implement.objectImpl["resolution"].content.number;
-            uint32_t channel = thisValue->implement.objectImpl["channe"].content.number;
+            uint32_t channel = thisValue->implement.objectImpl["channel"].content.number;
             uint8_t pin = args[0].content.number;
             ledcSetup(channel,freq,res);
             ledcAttachPin(pin,channel);
@@ -38,7 +38,7 @@ void InitPWMChannels(){
                 currentWorker->ThrowError("setDuty only accept number");
                 return VariableValue();
             }
-            uint32_t channel = thisValue->implement.objectImpl["channe"].content.number;
+            uint32_t channel = thisValue->implement.objectImpl["channel"].content.number;
             ledcWrite(channel,(uint32_t)args[0].content.number);
             return VariableValue();
         });
